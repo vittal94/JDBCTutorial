b8629a945b27193626179aedba523243aea7aab0
vittal94 <evg94z@mail.ru>
2024-12-04 21:25:14 +0300

    create scrollable rowset

---
+++ src/main/java/org/example/kotlinCode/ScrollableRowSet.kt
@@ -0,0 +1,43 @@
+package org.example.kotlinCode
+
+import java.sql.DriverManager
+import java.sql.ResultSet
+
+fun rowSet() {
+    DriverManager.getConnection(url, username, password).use { conn ->
+        conn.createStatement().use { stat ->
+            stat.executeUpdate("DROP TABLE IF EXISTS Books")
+            stat.executeUpdate("CREATE TABLE IF NOT EXISTS Books(" +
+                    "id INT AUTO_INCREMENT PRIMARY KEY," +
+                    "name CHAR(30) NOT NULL)")
+            stat.executeUpdate("INSERT INTO Books(name) VALUES('MAN')")
+            stat.executeUpdate("INSERT INTO Books(name) VALUES('JAVA')")
+            stat.executeUpdate("INSERT INTO Books(name) VALUES('SPRING')")
+        }
+        conn.prepareStatement(
+            "SELECT * FROM Books",
+            ResultSet.TYPE_SCROLL_INSENSITIVE,
+            ResultSet.CONCUR_READ_ONLY).use { preparedStatement ->
+            preparedStatement.executeQuery().use { resultSet ->
+                //reading the next element
+                if(resultSet.next()) println(resultSet.getString("name"))
+                if(resultSet.next()) println(resultSet.getString("name"))
+                //reading the previous element
+                if (resultSet.previous()) println(resultSet.getString("name"))
+                // reading relative to the current position forward
+                if(resultSet.relative(2)) println(resultSet.getString("name"))
+                //reading relative to the current position backward
+                if(resultSet.relative(-2)) println(resultSet.getString("name"))
+                //reading shifted from beginning
+                if(resultSet.absolute(2)) println(resultSet.getString("name"))
+                //reading first row
+                if(resultSet.first()) println(resultSet.getString("name"))
+                //reading last row
+                if(resultSet.last()) println(resultSet.getString("name"))
+            }
+        }
+    }
+}
+fun main() {
+rowSet()
+}
